6adc134a6f7d4f4193b8d3a96a6e0a8b61d24635
Om Patil <44ompatil@gmail.com>
2026-05-25 16:23:26 +0530

    feat : added traversal method for directory

--- src/1dedup/dirTraversal.py
+++ src/1dedup/dirTraversal.py
@@ -1,15 +1,36 @@
+import os
 from pathlib import Path
+from typing import Tuple
+
+from typing_extensions import List
 
 
 class dirTraversal:
 
 	def __init__(self, sourceDir : Path) -> None:
 
-		self.sourceDir = sourceDir
-	
-		if not self.sourceDir.is_dir():
-			raise NotADirectoryError(f"[PATH] : {sourceDir} is not a directory.")
+		self.sourceDir = Path(sourceDir)
 	
 		if not self.sourceDir.exists():
+			self.sourceDir = None
 			raise FileNotFoundError(f"[PATH] : {sourceDir} not found.")
+			
+		if not self.sourceDir.is_dir():
+			self.sourceDir = None
+			raise NotADirectoryError(f"[PATH] : {sourceDir} is not a directory.")
+	
+
+	def traverseDir(self) -> Tuple[list[Path], list[Path]]:
+
+		relPath = []
+		absPath = []
 		
+		for root, directory, files in os.walk(self.sourceDir):
+			rootPath = Path(root)
+			
+			for file in files:
+				fPath = rootPath / file
+				absPath.append(fPath.resolve())
+				relPath.append(fPath.relative_to(self.sourceDir))
+
+		return relPath, absPath
